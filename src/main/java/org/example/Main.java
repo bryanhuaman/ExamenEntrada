692560d469478e4ddd0544443dd94c99d301deff
bryan <bryanhuamanchavez29@gmail.com>
2024-04-03 09:14:24 -0500

    them.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,7 @@ import org.example.Clases.Gerente;
 
 import java.util.ArrayList;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
 
